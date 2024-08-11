6b5cebcc5db5dcbb155639bc72997deddc176477
Ishika Chauhan <152597760+ishika1880@users.noreply.github.com>
2024-08-11 16:10:11 +0530

    Update Main.java

--- src/main/java/com/oops/Interfaces/Main.java
+++ src/main/java/com/oops/Interfaces/Main.java
@@ -18,12 +18,12 @@ public class Main {
         B obj = new B();
         System.out.println(obj.isOdd(10));
     }
-    // infrace don't care whether 2 classes are realted or not 
+    // interface doesn't care whether 2 classes are related or not 
 
     // Engine car = new Car();
     // car.a - we can't access it because the variable access is defined by the Engine here
     // interface can also inherited
     //static interface method should always have a body
-    // methods that are overridden should can change the access specifier but should delare less restricted compared to the previous one 
-    //in nested interface the super interface should be public 
+    // methods that are overridden should change the access specifier but should be less restricted compared to the previous one 
+    //in the nested interface the super interface should be public 
 }
